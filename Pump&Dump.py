da8ce3b86f5959f90c66a482214f208bbb922b9e
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 23:13:21 +0530

    Update Pump&Dump.py

--- Pump&Dump.py
+++ Pump&Dump.py
@@ -1,1 +1,9 @@
+from binance.client import Client
+from binance.exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
+import requests
+import time
+import winsound
+
+# Replace your_api_key, your_api_secret with your api_key, api_secret
+client = Client(your_api_key, your_api_secret)
 

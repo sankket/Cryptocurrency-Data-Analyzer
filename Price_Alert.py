c8b9f3ccce26e01a6136480b6d11121df71cd744
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 23:11:22 +0530

    Update Price_Alert.py

--- Price_Alert.py
+++ Price_Alert.py
@@ -1,1 +1,12 @@
+import requests
+from prettytable import PrettyTable
+import time
+import winsound
+
+
+api = "https://api.binance.com/api/v1/ticker/allPrices" # The api for prices
+data = requests.get(api).json() # Calling the data from api
+
+symbols = [] # The list of symbols
+positions = [] # The list of positions
 

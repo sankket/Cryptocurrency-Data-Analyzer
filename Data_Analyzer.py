4c82ec9fe0d6867a219adc40e492b79b147b3b7a
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 20:57:10 +0530

    Update Data_Analyzer.py

--- Data_Analyzer.py
+++ Data_Analyzer.py
@@ -36,3 +36,47 @@ def calculate_data_list():
         counter += 1
     return symbols, volume, positions_final, price_change
 
+
+# Get candlestick data from Binance
+def get_kline():
+    symbols, volume, pozitii,price_change = calculate_data_list()
+    prices = []
+    prices1 = []
+    k=[]
+
+    for x in symbols:
+        try:
+            order = client.get_klines( # Get 1 minute candlestick data from server
+                symbol=x,
+                interval='1m')
+        except BinanceAPIException as e:
+            print (e.status_code)
+            print (e.message)
+        try:
+            order1 = client.get_klines( # Get 15 minute candlestick data from server
+                symbol=x,
+                limit= 1000,
+                interval='15m')
+        except BinanceAPIException as e:
+            print (e.status_code)
+            print (e.message)
+
+        if len(order1) < 970: # check if coin have at least 10 days of data
+            a = symbols.index(x) # get index of x in symbols
+            k.append(a)
+        else:
+            prices.append([]) # add empty list to list of 1 minute
+            prices1.append([]) # add empty list to list of 15 minutes
+            for i in range(len(order)):
+                prices[-1].append(float(order[i][1])) # save 1 minute data
+            for i in range(len(order1)):
+                prices1[-1].append(float(order1[i][1])) # save 15 minute data
+    k.reverse()
+
+    for x in k:
+        symbols.pop(x)
+        volume.pop(x)
+        all_positions.pop(x)
+        price_change.pop(x)
+
+    return symbols, volume, pozitii, prices, prices1,price_change

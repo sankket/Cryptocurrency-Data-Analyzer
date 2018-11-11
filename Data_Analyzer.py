9ea66cbafa341a4b911e6bc0184d87862759359c
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 20:56:18 +0530

    Update Data_Analyzer.py

--- Data_Analyzer.py
+++ Data_Analyzer.py
@@ -1,1 +1,10 @@
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+from binance.enums import *
+import time
+import threading
+import winsound
+
+# Replace your_api_key, your_api_secret with your api_key, api_secret
+client = Client(your_api_key, your_api_secret)
 

9e56729f7311800a9c565b589483ea088655abb8
Sanghapal Shelke <sanghapal25@gmail.com>
2018-11-11 20:45:34 +0530

    Update Binance_Hotkeys.py

--- Binance_Hotkeys.py
+++ Binance_Hotkeys.py
@@ -1,1 +1,13 @@
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+from binance.enums import *
+from binance.exceptions import BinanceAPIException, BinanceRequestException, BinanceWithdrawException
+from time import gmtime, strftime
+from pynput import keyboard
+
+# Replace your_api_key, your_api_secret with your api_key, api_secret
+client = Client(your_api_key, your_api_secret)
+
+
+data = client.get_exchange_info() # Retrieve exchange info from Binance
 
